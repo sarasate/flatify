30cc1775b5ccf5e7965df1987e108959a44de2e2
sarasate <luis@superlumen.io>
2020-02-26 13:43:45 +0100

    remove file

--- client/ui/components/calculator/calculator.js
+++ client/ui/components/calculator/calculator.js
@@ -2,7 +2,11 @@ import "./calculator.html";
 import "./controls.html";
 import "./heading.html";
 
-Template.calculator.onCreated(function () {});
+Template.calculator.onCreated(function() {
+  this.autorun(() => {
+    this.subscribe("templates.all");
+  });
+});
 
 Template.calculator.helpers({});
 

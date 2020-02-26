80c9775ff4f27f4b18624835c1ec306358a31eda
Pierre Portal <pierreportal@pierres-MBP.fritz.box>
2020-02-26 15:56:13 +0100

    add default data models for calculation

---
+++ imports/default.js
@@ -0,0 +1,83 @@
+let questions = [];
+
+const settings = {
+  type: "",
+  currency: "eur",
+  title: "M",
+  fullName: "Pierre Portal",
+  contractBase: 300,
+  addOns: {
+    clientCalls: 20,
+    copies: 10,
+    courtDay: 100, // * n
+    contractPage: 10, // * n
+    complexityMultiplier: 0.3 // + n (* cost),
+  }
+};
+
+const createQuestion = params => {
+  questions.push({ ...params });
+};
+
+const createOption = (questionId, params) => {
+  questions
+    .find(question => question._id === questionId)
+    .options.push({ ...params });
+};
+createQuestion({
+  _id: i,
+  user: "",
+  optional: true,
+  activated: true, // if .optional === true
+  type: "dropdown",
+  label: "Court",
+  options: [],
+  defaultValue: "Example of value",
+  steps: 3 // if .type === 'slider'
+});
+
+createOption(questionId, {
+  // label:'',
+  value: 10,
+  modifier: {
+    target: "base",
+    operator: "+",
+    expression: "input"
+  }
+});
+
+const addAddOn = newAddon => {
+  settings.addOns = { ...settings.addOns, ...newAddon };
+};
+
+const renderTemplate = (templateType, data) => {
+  console.log("TYPE:", templateType, "\nDATA:", data, "\n\n");
+  // Blaze.renderWithData(
+  //   Template[templateType],
+  //   data
+  //   Template.calculatorView
+  // )
+};
+// Map
+const createQuestions = questionList => {
+  return questionList.map(question => {
+    const { type } = question;
+    const { options } = question;
+    let data = { label: question.label };
+
+    const dataTypes = {
+      input: {
+        value: question.defaultValue,
+        placeholder: question.defaultValue
+      },
+      slider: { steps: Number((1 / question.steps).toFixed(1)) },
+      dropdown: { items: options },
+      checkbox: { options: options.map(o => ({ ...o, checked: "checked" })) }
+    };
+    return renderTemplate(type, { ...data, ...dataTypes[type] });
+  });
+};
+
+// addAddOn({ nightShift: 200 });
+
+// createQuestions(questions);

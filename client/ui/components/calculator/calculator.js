7a4be158a73fb800e1ed101d2accd3107bcdf697
Konstantin <bertram@lawlift.de>
2020-02-26 12:51:17 +0100

    move files to client

---
+++ client/ui/components/calculator/calculator.js
@@ -0,0 +1,9 @@
+import "./calculator.html";
+import "./controls.html";
+import "./heading.html";
+
+Template.calculator.onCreated(function () {});
+
+Template.calculator.helpers({});
+
+Template.calculator.events({});
--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,9 +2,9 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 // Import needed templates
-import '../../ui/layouts/body/body.js';
-import '../../ui/pages/home/home.js';
-import '../../ui/pages/not-found/not-found.js';
+import '../../ui/layouts/body/body';
+import '../../ui/pages/home/home';
+import '../../ui/pages/not-found/not-found';
 
 // Set up all routes in the app
 FlowRouter.route('/', {

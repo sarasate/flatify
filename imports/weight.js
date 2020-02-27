8664ea1433a02e82bc2effcbf2187c1d6cf1f207
sarasate <luis@superlumen.io>
2020-02-27 15:03:29 +0100

    add data

--- imports/weight.js
+++ imports/weight.js
@@ -1,43 +1,23 @@
-import { document } from "./document";
-import { court } from "./court";
-import { negotiation } from "./negotiation";
-import { weight } from "./weight";
-
-export const settings = {
-  type: "",
-  currency: "eur",
-  title: "M",
-  fullName: "Pierre Portal",
-  contractBase: 300,
-  addOns: {
-    clientCalls: 20,
-    copies: 10,
-    courtDay: 100, // * n
-    contractPage: 10, // * n
-    complexityMultiplier: 0.3 // + n (* cost),
-  }
-};
-
-export const standard = [
+export const weight = [
   {
-    label: "General",
+    label: "Value Adjustment",
     activated: true,
     noSwitch: true,
     questions: [
       {
-        _id: "3",
+        _id: "1",
         value: 0,
         selectedValue: null,
         optional: true,
         type: "range",
-        label: "Difficulty of clarification of facts",
+        label: "Economic Impact",
         options: [
           {
             value: "low",
             modifier: {
               target: "base",
               operator: "+",
-              expression: 0
+              expression: 10
             }
           },
           {
@@ -45,7 +25,42 @@ export const standard = [
             modifier: {
               target: "base",
               operator: "+",
-              expression: 300
+              expression: 20
+            }
+          },
+          {
+            value: "high",
+            modifier: {
+              target: "base",
+              operator: "+",
+              expression: 30
+            }
+          }
+        ],
+        defaultValue: 1
+      },
+      {
+        _id: "2",
+        value: 0,
+        selectedValue: null,
+        optional: true,
+        type: "range",
+        label: "Risk of liability",
+        options: [
+          {
+            value: "low",
+            modifier: {
+              target: "base",
+              operator: "+",
+              expression: 5
+            }
+          },
+          {
+            value: "medium",
+            modifier: {
+              target: "base",
+              operator: "+",
+              expression: 10
             }
           },
           {
@@ -58,13 +73,14 @@ export const standard = [
           }
         ]
       },
+
       {
-        _id: "4",
+        _id: "5",
         value: 0,
         selectedValue: null,
         optional: true,
         type: "range",
-        label: "Urgency",
+        label: "Legal Complexity",
         options: [
           {
             value: "low",
@@ -95,12 +111,4 @@ export const standard = [
       }
     ]
   }
-];
-
-export const categories = [
-  ...standard,
-  ...document,
-  ...court,
-  ...negotiation,
-  ...weight
 ];
